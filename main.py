ad85d2e8c553560bfcab6457a8c9fceb4fc441f1
potatso <potatso@protonmail.com>
2020-12-24 16:07:19 +0800

    完成复杂对象得读取，例如数据，class等

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+from queue import Queue
 from struct import *
 
 from Constants import Constants
@@ -36,6 +38,14 @@ class ObjectIO:
             stringBuilder += byte.decode()
         return stringBuilder
 
+    def readFloat(self):
+        num = self.readBytes(4)
+        return num
+
+    def readBoolean(self):
+        tc = int.from_bytes(self.readByte(), 'big')
+        return True if tc == 0 else False
+
     def writeBytes(self, value):
         self.base_stream.write(value)
 
@@ -55,14 +65,18 @@ class ObjectStream:
     def __init__(self, stream):
         self.bin = stream
         self.fieldStack = []
-        self.handles = {}
+        self.handles = []
         self.readStreamHeader()
+
+    def newHandles(self, obj):
+        self.handles.append(obj)
+        return len(self.handles) - 1 + Constants.baseWireHandle
 
     def readStreamHeader(self):
         magic = self.bin.readUnsignedShort()
         version = self.bin.readUnsignedShort()
         if magic != Constants.magic or version != Constants.version:
-            print("invalid bin header")
+            print(f"invalid bin header {magic:#2x} {version:#2x}")
             exit(-1)
 
     def readClassDescriptor(self):
@@ -70,11 +84,14 @@ class ObjectStream:
         读取非动态代理类的结构，目前还不支持动态代理的类
         :return:
         """
-        self.readClassDesc()
+        javaClass = self.__readClassDesc__()
+        # TODO: add classAnnotation to class structs
         self.readClassAnnotations()
-        self.readSuperClassDesc()
+        superjavaClass = self.readSuperClassDesc()
+        javaClass.superJavaClass = superjavaClass
+        return javaClass
 
-    def readClassDesc(self):
+    def __readClassDesc__(self):
         tc = self.bin.readByte()
         if tc != Constants.TC_CLASSDESC:
             print("InternalError")
@@ -95,6 +112,10 @@ class ObjectStream:
         print(f"className {className}")
         print(f"suid {suid}")
         print(f"number of fields {numFields}")
+        classDesc = JavaClass(className, suid, flags)
+        classDesc.hasWriteObjectData = hasWriteObjectData
+        classDesc.hasBlockExternalData = hasBlockExternalData
+        self.newHandles(classDesc)
         fields = []
         for i in range(numFields):
             tcode = self.bin.readByte()
@@ -102,12 +123,15 @@ class ObjectStream:
             if tcode == b'L' or tcode == b'[':
                 signature = self.readTypeString()
             else:
-                signature = tcode
+                signature = tcode.decode()
             fields.append({'name': fname, 'sinnature': signature})
             print(f"name {fname} sinnature {signature}")
-        lastFieldStack = self.fieldStack.pop()
-        lastFieldStack.append(fields)
-        self.fieldStack.append(lastFieldStack)
+        if self.fieldStack and fields:
+            lastFieldStack = self.fieldStack.pop()
+            lastFieldStack.append(fields)
+            self.fieldStack.append(lastFieldStack)
+            classDesc.fields = fields
+        return classDesc
 
     def readClassAnnotations(self):
         """
@@ -121,16 +145,19 @@ class ObjectStream:
         print(f"ClassAnnotations end ")
 
     def readSuperClassDesc(self):
-        '''
+        """
         读取父类的的class信息，一直到父类为空，类似于链表。java不支持多继承
         :return:
-        '''
+        """
         tc = self.bin.peekByte()
         print(f"Super Class start")
         if tc != Constants.TC_NULL:
-            self.readContent()
-        self.bin.readByte()
+            superJavaClass = self.readContent()
+        else:
+            self.bin.readByte()
+            superJavaClass = None
         print(f"Super Class End")
+        return superJavaClass
 
     def readObject(self):
         tc = self.bin.readByte()
@@ -140,89 +167,271 @@ class ObjectStream:
             return
         tc = self.bin.peekByte()
         if tc == Constants.TC_CLASSDESC:
-            self.readClassDescriptor()
-            self.readClassData()
+            javaClass = self.__readClassDesc__()
+            # TODO: add classAnnotation to class structs
+            self.readClassAnnotations()
+            superjavaClass = self.readSuperClassDesc()
+            javaClass.superJavaClass = superjavaClass
+            javaObject = JavaObject(javaClass)
+            self.newHandles(javaObject)
+            self.readClassData(javaObject)
         elif tc == Constants.TC_NULL:
-            pass
+            return self.readNull()
         elif tc == Constants.TC_REFERENCE:
-            self.readHandle()
+            javaClass = self.readHandle()
+            javaObject = JavaObject(javaClass)
+            self.newHandles(javaObject)
+            self.fieldStack.append([javaClass.fields])
+            self.readClassData(javaObject)
         elif tc == Constants.TC_PROXYCLASSDESC:
             pass
         else:
             printInvalidTypeCode(tc)
 
-    def readClassData(self):
+        return javaObject
+
+    def readClassData(self, javaObject):
         """
         读取对象的值，先读取父类的值，再读取子类的值
         :return:
         """
         fieldStack = self.fieldStack.pop()
         while len(fieldStack):
-            field = fieldStack.pop()
-            print(field)
+            fields = fieldStack.pop()
+            currentField = []
+            for field in fields:
+                singature = field['sinnature']
+                if singature.startswith('L') or singature.startswith('['):
+                    value = self.readContent()
+                elif singature == 'I':
+                    value = self.bin.readInt()
+                elif singature == 'F':
+                    value = self.bin.readFloat()
+                elif singature == "Z":
+                    value = self.bin.readBoolean()
+                print(f"name {field['name']}  value {value}")
+                currentField.append({field['name']: [value, field['sinnature']]})
+            javaObject.fields.put(currentField)
+
+        if javaObject.javaClass.hasWriteObjectData:
+            self.readObjectAnnotations(javaObject)
 
     def readHandle(self):
         """
         反序列化中是不会出现两个一摸一样的值，第二个值一般都是引用
         :return:
         """
+        tc = self.bin.readByte()
         handle = self.bin.readInt()
-        # handle = handle-Constants.baseWireHandle
         print(hex(handle))
-        return hex(handle)
+        handle = handle - Constants.baseWireHandle
+        obj = self.handles[handle]
+        if isinstance(obj, JavaClass):
+            return obj
+        elif isinstance(obj, JavaString):
+            return obj.string
+        elif isinstance(obj, JavaObject):
+            return obj
+        else:
+            print("unsuppprt type")
+            return None
 
     def readTypeString(self):
-        tc = self.bin.readByte()
+        tc = self.bin.peekByte()
         if tc == Constants.TC_NULL:
             pass
         elif tc == Constants.TC_REFERENCE:
             return self.readHandle()
         elif tc == Constants.TC_STRING:
-            return self.bin.readString()
+            return self.readString()
         elif tc == Constants.TC_LONGSTRING:
-            return self.bin.readString()
+            return self.readString()
         else:
             printInvalidTypeCode(tc)
+
+    def readString(self):
+        tc = self.bin.readByte()
+        string = self.bin.readString()
+        self.newHandles(JavaString(string))
+        return string
 
     def readContent(self):
         tc = self.bin.peekByte()
         if tc == Constants.TC_NULL:
-            return 'null'
+            return self.readNull()
         elif tc == Constants.TC_REFERENCE:
-            pass
+            return self.readHandle()
         elif tc == Constants.TC_CLASS:
-            pass
+            self.bin.readByte()
+            return self.readClassDescriptor()
         elif tc == Constants.TC_CLASSDESC or tc == Constants.TC_PROXYCLASSDESC:
-            self.readClassDescriptor()
+            return self.readClassDescriptor()
         elif tc == Constants.TC_STRING or tc == Constants.TC_LONGSTRING:
-            pass
-        elif tc == Constants.TC_ARRAY:
-            pass
+            return self.readTypeString()
         elif tc == Constants.TC_ENUM:
             pass
         elif tc == Constants.TC_OBJECT:
-            self.readObject()
+            return self.readObject()
         elif tc == Constants.TC_EXCEPTION:
             pass
         elif tc == Constants.TC_ARRAY:
-            pass
+            return self.readArray()
         elif tc == Constants.TC_BLOCKDATA:
-            pass
+            return self.readBlockData()
         elif tc == Constants.TC_BLOCKDATALONG:
             pass
         elif tc == Constants.TC_ENDBLOCKDATA:
             print("end")
+            self.bin.readByte()
             return 'end'
         else:
             printInvalidTypeCode(tc)
             exit(-1)
+
+    def readBlockData(self):
+        tc = self.bin.readByte()
+        length = int.from_bytes(self.bin.readByte(), 'big')
+        data = self.bin.readBytes(length)
+        print(data)
+        return data
+
+    def readObjectAnnotations(self, javaObject):
+        print("reading readObjectAnnotations")
+        while True:
+            obj = self.readContent()
+            print(obj)
+            if obj == 'end':
+                break
+            else:
+                javaObject.objectAnnotation.append(obj)
+
+    def readNull(self):
+        tc = self.bin.readByte()
+        return 'null'
+
+    def readArray(self):
+        self.bin.readByte()
+        tc = self.bin.peekByte()
+        if tc == Constants.TC_CLASSDESC:
+            javaClass = self.readClassDescriptor()
+        elif tc == Constants.TC_REFERENCE:
+            javaClass = self.readHandle()
+        else:
+            print("unsupport type")
+        size = self.bin.readInt()
+        print(javaClass)
+        print(f"array size {size}")
+        array = []
+        print(hex(self.newHandles(array)))
+        for i in range(size):
+            obj = self.readContent()
+            if obj != 'end':
+                array.append(obj)
+            else:
+                break
+        return array
 
 
 def printInvalidTypeCode(code: bytes):
     print(f"invalid type code {int.from_bytes(code, 'big'):#2x}")
 
 
+class JavaClass:
+    def __init__(self, name, suid, flags):
+        self.name = name
+        self.suid = suid
+        self.flags = flags
+        self.superJavaClass = None
+        self.fields = []
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class JavaString:
+    def __init__(self, string):
+        self.string = string
+
+    def __str__(self):
+        return self.string
+
+
+class JavaObject:
+    def __init__(self, javaClass):
+        # TODO 必须定义父类的字段如何存储，如果都缠在一起，在写入的时候就会没有先后顺序之分
+        self.javaClass = javaClass
+        # fields 保存类的字段，队列数据结构。父类在最前，子类在最后
+        self.fields = Queue()
+        self.objectAnnotation = []
+
+    def __str__(self):
+        return f"className {self.javaClass.name}\t extend {self.javaClass.superJavaClass}"
+
+
+def javaClass2Yaml(javaClass):
+    d = {'suid': javaClass.suid, 'flags': javaClass.flags,
+         'classAnnotation': None}
+    # TODO classAnnotation
+    if javaClass.superJavaClass:
+        d['superClass'] = javaClass2Yaml(javaClass.superJavaClass)
+    else:
+        d['superClass'] = None
+    return {javaClass.name: d}
+
+
+def javaObject2Yaml(javaObject):
+    d = {'suid': javaObject.javaClass.suid, 'flags': javaObject.javaClass.flags,
+         'classAnnotation': None}
+    # TODO classAnnotation
+    if javaObject.javaClass.superJavaClass:
+        d['superClass'] = javaClass2Yaml(javaObject.javaClass.superJavaClass)
+    else:
+        d['superClass'] = None
+    fields = []
+    while javaObject.fields.qsize():
+        currentObjFields = javaObject.fields.get()
+        for currentObjField in currentObjFields:
+            for k, v in currentObjField.items():
+                data = {'type': v[1], 'fieldName': k}
+                if isinstance(v[0], JavaObject):
+                    data['value'] = javaObject2Yaml(v[0])
+                elif isinstance(v[0], list):
+                    valueList = []
+                    for o in v[0]:
+                        if isinstance(o, JavaObject):
+                            valueList.append(javaObject2Yaml(o))
+                    data['value'] = valueList
+                else:
+                    data['value'] = v[0]
+                fields.append({'data': data})
+
+    d['Fields'] = fields
+    objectAnnotation = []
+    for o in javaObject.objectAnnotation:
+        if isinstance(o, JavaObject):
+            o = javaObject2Yaml(o)
+        objectAnnotation.append(o)
+    if objectAnnotation:
+        d['objectAnnotation'] = objectAnnotation
+    else:
+        d['objectAnnotation'] = None
+    return {javaObject.javaClass.name: d}
+
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, bytes):
+            return obj.hex()
+        return json.JSONEncoder.default(self, obj)
+
+
 if __name__ == '__main__':
-    f = open("test.ser", "rb")
+    f = open("payload.out", "rb")
     s = ObjectIO(f)
-    ObjectStream(s).readContent()
+    obj = ObjectStream(s).readContent()
+    print(obj)
+    d = javaObject2Yaml(obj)
+    print("------------------------------------")
+    print(d)
+    print("------------------------------------")
+    print(json.dumps(d, indent=4, cls=MyEncoder, ensure_ascii=False))
